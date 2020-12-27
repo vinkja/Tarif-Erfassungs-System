5cdce68b39ab3760f44ef7002ae11e1295bc40c4
Sascha <sascha.fluor@gmx.net>
2020-12-27 16:06:02 +0100

    First Dropdown version Operators / Years

--- src/app/view.js
+++ src/app/view.js
@@ -3,11 +3,12 @@
 const url = "http://ela-prod-01.eturnity.io/"
 const apiOperator = "lea_settings/api/operators"
 
-
 // fetch(url+apiOperator)
 //     .then(response => response.json())
 //     .then(json => console.log(json))
 //     .catch(err => console.log(err))
+
+// Operator Dropdown
 
 fetch(url+apiOperator)
     .then(function (response) {
@@ -22,9 +23,21 @@ fetch(url+apiOperator)
 
 function appendData(data) {
     let mainContainer = document.getElementById("energieversoger");
-    for (var i = 1; i < data.length; i++) {
+    for (let i = 1; i < data.length; i++) {
         let option = document.createElement("option");
         option.innerHTML = data[i].name;
         mainContainer.appendChild(option);
     }
+}
+
+//Actual Year Dropdown
+
+let actualYear = new Date().getFullYear();
+maxDate = actualYear + 1;
+select = document.getElementById('year');
+
+for (let i = actualYear; i <= maxDate; i++){
+    let opt = document.createElement('option');
+    opt.innerHTML = i;
+    select.appendChild(opt);
 }
